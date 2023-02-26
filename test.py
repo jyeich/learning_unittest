f3dc7217156e6a9d8cb44fe199a5944c772ea5e2
Jason <jasonyeich@gmail.com>
2023-02-26 16:39:16 -0500

    testing

---
+++ test.py
@@ -0,0 +1,20 @@
+import unittest
+
+from my_sum import sum
+
+
+class TestSum(unittest.TestCase):
+    def test_list_int(self):
+
+        #Test that it can sum a list of integers
+
+        data = [1, 2, 3]
+        result = sum(data)
+        self.assertEqual(result, 6)
+    def test_two(self):
+        data = [2, 2, 2]
+        result = sum(data)
+        self.assertEqual(result, 6)
+
+if __name__ == '__main__':
+    unittest.main()
